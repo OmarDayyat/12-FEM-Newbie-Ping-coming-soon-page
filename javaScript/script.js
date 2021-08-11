461e3320be058897750f992312747c7af6243f4f
Omar Al-Dayyat <jakezooz999@gmail.com>
2021-08-11 12:18:24 +0300

    Start and End of project

---
+++ javaScript/script.js
@@ -0,0 +1,29 @@
+const email = document.getElementById("email");
+const form = document.getElementById("form");
+const errorD = document.querySelector(".error-msg-desktop");
+
+const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+form.addEventListener('submit', (e) => {
+  let msgs = [];
+
+  if (email.value === "" || email.value == null) {
+    errorD.style.display = "block";
+    msgs.push("Email can't be empty");
+  }
+
+  if (!email.value.match(regex) && email.value !== "" && email.value != null) {
+    errorD.style.display = "block";
+    msgs.push("Please provide a valid email address");
+  }
+
+  if (msgs.length > 0) {
+    e.preventDefault();
+    errorD.innerText = msgs.join(',');
+  }
+
+
+  // console.log(msgs[0])
+
+})
+
